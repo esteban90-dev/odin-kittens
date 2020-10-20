bf76c470c08bc50494dc34476dc1fc97872e805a
Stephen Allard <stephencallard@gmail.com>
2020-10-20 17:22:40 -0500

    Add 7 restful actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,52 @@
 class KittensController < ApplicationController
+  before_action :set_kitten, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @kittens = Kitten.all
+  end
+
+  def show
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = "New kitten created!"
+      redirect_to kittens_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten modified!"
+      redirect_to kittens_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @kitten.destroy
+    flash[:notice] = "Kitten deleted!"
+    redirect_to kittens_path
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+  def set_kitten
+    @kitten = Kitten.find(params[:id])
+  end
 end
